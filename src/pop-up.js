a0a3f8e8bc057ddea4198af71e620b70f7c453e4
Riririsu-Kokoromu <therese.sjdyka@gmail.com>
2024-03-20 16:58:48 +0800

    添加 环形进度条

--- src/pop-up.js
+++ src/pop-up.js
@@ -1,23 +1,48 @@
+function startProgress(ms) {
+    const progressCircle = document.querySelector(".progress-circle");
+    const progressBar = progressCircle.querySelector(".progress-bar");
+    let circumference = progressBar.getTotalLength();
+    progressBar.style.strokeDasharray = circumference + " " + circumference;
+
+    let progress = 0;
+    let interval = setInterval(function () {
+        if (progress >= 100) {
+            clearInterval(interval);
+        } else {
+            progress++;
+            var dashOffset = circumference - progress / 100 * circumference;
+            progressBar.style.strokeDashoffset = dashOffset;
+        }
+    }, ms / 105);
+}
+
 popup = (text, ms) => {
     // 判断popup元素是否存在
     let popupElements = document.getElementsByClassName('popup');
     if (popupElements.length === 0 && (text || text === '')) {
         const popup = document.createElement('div');
         popup.className = 'popup';
-        popup.innerHTML = text;
+        popup.innerHTML = `
+            <div class="progress-circle">
+                <svg viewBox="0 0 100 100">
+                    <circle class="progress-bar" cx="50" cy="50" r="40" fill="none" />
+                </svg>
+            </div>
+        ` + text;
         document.body.append(popup);
 
         let popupWidth = popup.offsetWidth;
         popup.style.left = 'calc(50% - ' + popupWidth / 2 + 'px)';
 
-        setTimeout(() => {
-            popup.style.bottom = '50px';
-        }, 50);
-
         // 判断是否有时间参数，如果没有设置为3000ms
         if (!ms || ms === '') {
             ms = 3000;
         }
+
+        setTimeout(() => {
+            popup.style.bottom = '50px';
+            startProgress(ms)
+        }, 50);
 
         setTimeout(() => {
             popup.style.bottom = '100px';
